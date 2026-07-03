cb797320b665bd4b6cdc8d7abdeb21bb911c113a
ELIEL VINÍCIUS VIEIRA DE LIMA <eliel_v_lima@estudante.sesisenai.org.br>
2026-07-03 16:28:22 -0300

    Data types and typeof

---
+++ Sintax - 1/Data type and typeof/types.js
@@ -0,0 +1,17 @@
+let numero = 1;
+let numeroDec = 0.1;
+let eliel = "eliel";
+let boolean = true;
+let undefined;
+
+typeof(numero);
+typeof(numeroDec);
+typeof(eliel);
+typeof(boolean);
+typeof(undefined);
+
+console.log(typeof(numero));
+console.log(typeof(numeroDec));
+console.log(typeof(eliel));
+console.log(typeof(boolean));
+console.log(typeof(undefined));

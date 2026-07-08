8c1dd518a67b00922220b253d47f32e9815954d0
ELIEL VINÍCIUS VIEIRA DE LIMA <eliel_v_lima@estudante.sesisenai.org.br>
2026-07-08 13:54:34 -0300

    == vs ===

---
+++ Sintax - 1/== vs ===/program.js
@@ -0,0 +1,6 @@
+console.log(5 == "5"); // True
+console.log(5 === "5"); // False
+console.log(0 == false); // True
+console.log(0 === false); // False
+console.log(null == undefined); // True
+console.log(null === undefined); // False

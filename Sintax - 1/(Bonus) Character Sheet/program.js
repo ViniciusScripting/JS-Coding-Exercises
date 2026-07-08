36c283ae640a6285881c7537ca8cfcd0a815a907
ELIEL VINÍCIUS VIEIRA DE LIMA <eliel_v_lima@estudante.sesisenai.org.br>
2026-07-08 14:09:08 -0300

    The tale of Pintosvaldo

---
+++ Sintax - 1/(Bonus) Character Sheet/program.js
@@ -0,0 +1,8 @@
+const name = "Pintosvaldo";
+const atribute = "Pinto Warrior";
+let level = 67;
+let hp = 67000;
+let alive = true;
+let equipment = "Tung tung tung sahur's baseball bat";
+
+console.log(`${name} is a ${atribute} level ${level} with ${hp} HP. Equipment ${equipment}. Stats: ${alive}`)

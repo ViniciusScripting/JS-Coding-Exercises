01ce7dcae690c424422ab666ac11e21b43db7b78
ELIEL VINÍCIUS VIEIRA DE LIMA <eliel_v_lima@estudante.sesisenai.org.br>
2026-07-08 13:59:39 -0300

    camelCase

---
+++ Sintax - 1/Naming Variables Correctly/program.js
@@ -0,0 +1,14 @@
+// Before:
+const primeiro_nome = "João";
+const DataNascimento = "01/01/2000";
+const SALARIO = 3000;
+let numerodetentativas = 0;
+const NomeCompleto = "João Silva";
+
+// After:
+
+const primeiroNome = "João";
+const dataNascimento = "01/01/2000";
+const salario = 3000;
+let numeroDeTentativas = 0;
+const nomeCompleto = "João Silva";

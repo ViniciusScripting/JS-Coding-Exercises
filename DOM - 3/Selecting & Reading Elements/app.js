448a2f700f834042f73c2391ee14f179267659c2
ELIEL VINÍCIUS VIEIRA DE LIMA <eliel_v_lima@estudante.sesisenai.org.br>
2026-07-10 16:04:07 -0300

    Selecting & Reading

---
+++ DOM - 3/Selecting & Reading Elements/app.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector('#titulo');
+const descricao = document.querySelector('.descricao');
+const botao = document.querySelector('#btn')
+
+console.log(titulo.textContent);
+console.log(descricao.textContent)
+console.log(botao.tagName)
